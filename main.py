1a61054f7aa03a9308b3e7056d0bb8666c9db5d1
Masterain <i@irain.in>
2023-09-15 18:09:43 -0700

    Update main.py
    remove duplicated code

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import httpx
 import os
 
+resolution_set = [[1440, 3120], [1, 1], [1152, 2048]]
+
+
+def url_process(url: str) -> tuple:
+    url = url.split("/")
+    return url[-1], url[-2], url[-3], url[-4]
+
 
 def get_cn_background():
     url = ("https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/content?filter_adv=true&key=eYd89JmJ&language=zh-cn"
            "&launcher_id=18")
     response = httpx.get(url).json()
     background_url = response["data"]["adv"]["background"]
-    file_name = background_url.split("/")[-1]
-    day = background_url.split("/")[-2]
-    month = background_url.split("/")[-3]
-    year = background_url.split("/")[-4]
+    file_name, day, month, year = url_process(background_url)
     os.makedirs(f"./output/cn/{year}/{month}/{day}/", exist_ok=True)
     with open(f"./output/cn/{year}/{month}/{day}/{file_name}", "wb") as f:
         f.write(httpx.get(background_url).content)
@@ -21,10 +25,7 @@ def get_bilibili_background():
            "&launcher_id=17")
     response = httpx.get(url).json()
     background_url = response["data"]["adv"]["background"]
-    file_name = background_url.split("/")[-1]
-    day = background_url.split("/")[-2]
-    month = background_url.split("/")[-3]
-    year = background_url.split("/")[-4]
+    file_name, day, month, year = url_process(background_url)
     os.makedirs(f"./output/bilibili/{year}/{month}/{day}/", exist_ok=True)
     with open(f"./output/bilibili/{year}/{month}/{day}/{file_name}", "wb") as f:
         f.write(httpx.get(background_url).content)
@@ -40,10 +41,7 @@ def get_os_background():
         response = httpx.get(url).json()
         try:
             background_url = response["data"]["adv"]["background"]
-            file_name = background_url.split("/")[-1]
-            day = background_url.split("/")[-2]
-            month = background_url.split("/")[-3]
-            year = background_url.split("/")[-4]
+            file_name, day, month, year = url_process(background_url)
             os.makedirs(f"./output/os/{language}/{year}/{month}/{day}/", exist_ok=True)
             with open(f"./output/os/{language}/{year}/{month}/{day}/{file_name}", "wb") as f:
                 f.write(httpx.get(background_url).content)
@@ -52,22 +50,17 @@ def get_os_background():
 
 
 def get_cn_cloud():
-    resolution_set = [[1440, 3120], [1, 1], [1152, 2048]]
     for r in resolution_set:
         url = "https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/getUIConfig?height=%s&width=%s" % (r[0], r[1])
         response = httpx.get(url).json()
         background_url = response["data"]["bg_image"]["url"]
-        file_name = background_url.split("/")[-1]
-        day = background_url.split("/")[-2]
-        month = background_url.split("/")[-3]
-        year = background_url.split("/")[-4]
+        file_name, day, month, year = url_process(background_url)
         os.makedirs(f"./output/cloud_cn/{year}/{month}/{day}/", exist_ok=True)
         with open(f"./output/cloud_cn/{year}/{month}/{day}/{file_name}", "wb") as f:
             f.write(httpx.get(background_url).content)
 
 
 def get_os_sg_cloud():
-    resolution_set = [[1440, 3120], [1, 1], [1152, 2048]]
     for r in resolution_set:
         url = "https://sg-cg-api.hoyoverse.com/hk4e_global/cg/gamer/api/getUIConfig?height=%s&width=%s" % (r[0], r[1])
         headers = {
@@ -75,10 +68,7 @@ def get_os_sg_cloud():
         }
         response = httpx.get(url, headers=headers).json()
         background_url = response["data"]["bg_image"]["url"]
-        file_name = background_url.split("/")[-1]
-        day = background_url.split("/")[-2]
-        month = background_url.split("/")[-3]
-        year = background_url.split("/")[-4]
+        file_name, day, month, year = url_process(background_url)
         os.makedirs(f"./output/cloud_sg/{year}/{month}/{day}/", exist_ok=True)
         with open(f"./output/cloud_sg/{year}/{month}/{day}/{file_name}", "wb") as f:
             f.write(httpx.get(background_url).content)
