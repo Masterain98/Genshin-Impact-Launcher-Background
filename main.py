78afa6ce6b877ffd3f92296ed910cd596b01c1e2
Masterain <i@irain.in>
2024-04-07 16:04:46 -0700

    Adapt Python 12 (#1)
    * Update to Python 12
    
    * code style
    
    * Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import httpx
 import os
 
-resolution_set = [[1440, 3120], [1, 1], [1152, 2048]]
+RESOLUTION_SET = [[1440, 3120], [1, 1], [1152, 2048]]
+RETRY_TIMES = 3
 
 
 def url_process(url: str) -> tuple:
@@ -10,9 +11,19 @@ def url_process(url: str) -> tuple:
 
 
 def get_cn_background():
+    error_message = ""
     url = ("https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/content?filter_adv=true&key=eYd89JmJ&language=zh-cn"
            "&launcher_id=18")
-    response = httpx.get(url).json()
+    for _ in range(RETRY_TIMES):
+        try:
+            response = httpx.get(url, timeout=15).json()
+            break
+        except (httpx.ReadTimeout, httpx.RemoteProtocolError, httpx.ConnectTimeout) as e:
+            error_message += f"```{str(e)}```\n"
+            continue
+    else:
+        print(f"::error title=Failed to cache CN background::{error_message}")
+        return None
     background_url = response["data"]["adv"]["background"]
     file_name, day, month, year = url_process(background_url)
     os.makedirs(f"./output/cn/{year}/{month}/{day}/", exist_ok=True)
@@ -50,7 +61,7 @@ def get_os_background():
 
 
 def get_cn_cloud():
-    for r in resolution_set:
+    for r in RESOLUTION_SET:
         url = "https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/getUIConfig?height=%s&width=%s" % (r[0], r[1])
         response = httpx.get(url).json()
         background_url = response["data"]["bg_image"]["url"]
@@ -61,7 +72,7 @@ def get_cn_cloud():
 
 
 def get_os_sg_cloud():
-    for r in resolution_set:
+    for r in RESOLUTION_SET:
         url = "https://sg-cg-api.hoyoverse.com/hk4e_global/cg/gamer/api/getUIConfig?height=%s&width=%s" % (r[0], r[1])
         headers = {
             "x-rpc-cg_game_biz": "hk4e_global",
@@ -83,12 +94,12 @@ def try_all_resolution():
     background_url_list = []
     log = open("log.txt", "w")
 
-    def process_resolution(i, j):
-        url = "https://sg-cg-api.hoyoverse.com/hk4e_global/cg/gamer/api/getUIConfig?height=%s&width=%s" % (i, j)
+    def process_resolution(x, y):
+        url = "https://sg-cg-api.hoyoverse.com/hk4e_global/cg/gamer/api/getUIConfig?height=%s&width=%s" % (x, y)
         response = client.get(url).json()
         background_url = response["data"]["bg_image"]["url"]
-        print(f"Resolution: {i}x{j}, URL: {background_url}")
-        log.write(f"Resolution: {i}x{j}, URL: {background_url}\n")
+        print(f"Resolution: {x}x{y}, URL: {background_url}")
+        log.write(f"Resolution: {x}x{y}, URL: {background_url}\n")
         background_url_list.append(background_url)
 
     num_cores = multiprocessing.cpu_count()
@@ -114,6 +125,8 @@ def try_all_resolution():
 
 
 def mys_wallpaper():
+    print("::group::Checking MYS wallpaper")
+    error_message = ""
     api_url = ("https://hk4e-api.mihoyo.com/event/contenthub/v1/wall_papers?page={page_number}&size=100&type={type}&ba"
                "dge_uid=100000000&badge_region=cn_qd01&game_biz=hk4e_cn&lang=zh-cn")
     wallpaper_type_list = [
@@ -137,13 +150,28 @@ def mys_wallpaper():
                 for url in wallpaper_url_list:
                     file_name, day, month, year = url_process(url)
                     os.makedirs(f"./output/mys/{w['name']}/{wallpaper_title}/{year}/{month}/{day}/", exist_ok=True)
+                    # check if file exists
+                    if os.path.exists(f"./output/mys/{w['name']}/{wallpaper_title}/{year}/{month}/{day}/{file_name}"):
+                        continue
                     with open(f"./output/mys/{w['name']}/{wallpaper_title}/{year}/{month}/{day}/{file_name}",
                               "wb") as f:
-                        f.write(httpx.get(url).content)
+                        print(f"Downloading {w['name']}/{wallpaper_title}/{year}/{month}/{day}/{file_name}...")
+                        for _ in range(RETRY_TIMES):
+                            try:
+                                conn = httpx.get(url)
+                                break
+                            except (httpx.ReadTimeout, httpx.RemoteProtocolError, httpx.ConnectTimeout) as e:
+                                error_message += f"```{str(e)}```\n"
+                                continue
+                        else:
+                            print(f"::error title=Failed to cache MYS wallpaper::{error_message}")
+                            return None
+                        f.write(conn.content)
             if not response["data"]["has_more"]:
                 break
             else:
                 page_number += 1
+    print("::endgroup::")
 
 
 def main():
